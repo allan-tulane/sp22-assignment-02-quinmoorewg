626cb1bac7e0a7a27f5ab8f2b500171079c40cec
quinmoore <71909979+quinmoorewg@users.noreply.github.com>
2022-05-16 17:28:51 +0000

    Assignment 2

--- assignment-02.md
+++ assignment-02.md
@@ -1,6 +1,6 @@
 # CMPS 2200 Assignment 2
 
-**Name:**_________________________
+**Name:**___Quin Moore______________________
 
 In this assignment we'll work on applying the methods we've learned to analyze recurrences, and also see their behavior
 in practice. As with previous
@@ -11,55 +11,146 @@ and push to your github repository.
 
 
 1. Derive asymptotic upper bounds of work for each recurrence below.
-  * $W(n)=2W(n/3)+1$
-.  
+  * $W(n)=2W(n/3)+1$ k = 1
+  * $W(n/3)=2W(n/9)+1$
+  * $W(n)=2*2W(n/9)+1+1$ k = 2 
+  * $W(n/9)=2W(n/27)+1$
+  * $W(n)=2*2*2W(n/27)+1+1+1$ k = 3
+  * $W(n)=2^k*W(n/3^k)+k
+  * n = 3^k
+  * log3(n) = k
+  * W(n)=2^log3(n)*W(1)+log3(n)
+  * Asymptotic upper bound is 2^log3(n)
+.  Big O(n)
+   * height is  log3𝑛 
+  
+* num nodes at level  𝑖  is  2𝑖 
+
+* num nodes at level  log3𝑛  is  2log3𝑛∈𝑂(𝑛𝑙𝑜𝑔32)
 .  
 .  
 .  
 .  
   * $W(n)=5W(n/4)+n$
+  * W(n/4)=5W(n/16)+n
+  * W(n)=5*5W(n/16)+n+n
+  * W(n/16)=5W(n/64)+n
+  * W(n)=5*5*5W(n/64)+n+n+n
+  * W(n)=5^k*W(n/4^k)+k*n
+  * n=4^k
+  * log4(n)=k
+  * W(n)=5^log4(n)*W(1)+log4(n)*n
+  * Aymptotic upper bound is log4(n)*n
+  * Big O(nlog(n))
+  * num levels is  log4𝑛
+  * num nodes at level  𝑖  is  5𝑖
+  * ⇒5log4𝑛∈𝑂(𝑛log45)
 .  
 .  
 .  
 .  
 .  
   * $W(n)=7W(n/7)+n$
-.  
+  * W(n/7)=7W(n/49)+n
+  * W(n)=7*7W(n/49)+n+n
+  * W(n)=7^k*W(n/7^k)+k*n
+  * n=7^k
+  * log7(n)=k
+  * W(n)=7^log7(n)*W(1)+log7(n)*n
+  * W(n)=n+log7(n)*n
+  * Aymptotic upepr bound is log7(n)*n
+.   Big O(nlog(n))
+    * max cost per level is
+    * 𝑛 num levels =  log7𝑛
+    * ⇒𝑂(𝑛log7𝑛)
+    
 .  
 .  
 .  
 .  
   * $W(n)=9W(n/3)+n^2$
+  * W(n/3) = 9W(n/9)+n^2
+  * W(n)=9*9W(n/9)+n^2+n^2
+  * W(n)=9^kW(n/3^k)+kn^2
+  * n = 3^k
+  * log3(n)=k
+  * W(n)=9^log3(n)*W(1)+log(n)*n^2
+  * Aymptotic upper bound is log(n)*n^2
+  * max cost per level is  𝑛2
+  * num levels =  log3𝑛
+  * ⇒𝑂(𝑛2log3𝑛)
 .  
 .  
 .  
 .  
 .  
   * $W(n)=8W(n/2)+n^3$
+  * W(n/2)=8W(n/4)+n^3
+  * W(n) = 8*8W(n/4)+n^3+n^3
+  * W(n) = 8^k*W(n/2^k)+kn^3
+  * log2(n)=k
+  * W(n) = 8^log2(n)*W(1)+log2(n)*n^3
+  * W(n)=4n+log(n)*n^3
+  * Aysmptotic upper bound is log(n)*n^3
+  * 𝑂(𝑛3log2𝑛)  .
 .  
 .  
 .  
 .  
 .  
   * $W(n)=49W(n/25)+n^{3/2}\log n$
+  * W(n/25)=49W(n/625) + n^3/2 * log(n)
+  * W(n)=49*49W(n/625) + 2(n^3/2 * log(n))
+  * W(n)=49^k*W(n/25^k)+k(n^3/2*log(n))
+  * log25(n)=k
+  * W(n)=49^log25(n)*W(1)+log25(n)*(n^3/2*log(n))
+  * Asympototic upper bound is log25(n)*(n^3/2*log(n)
+  * 𝑂(𝑛3/2log𝑛)
 .  
 .  
 .  
 .  
 .  
   * $W(n)=W(n-1)+2$
+  * W(n-1)=W(n-2)+2
+  * W(n)=W(n-2)+2+2
+  * W(n-2)=W(n-3)+2
+  * W(n)=W(n-3)+2+2+2
+  * W(n)=W(n-k)+2k
+  * n=k
+  * W(n)=W(1)+2n
+  * Asymptotic upper bound is 2n
+  * 𝑂(𝑛)
 .  
 .  
 .  
 .  
 .  
   * $W(n)= W(n-1)+n^c$, with $c\geq 1$
+  * W(n-1)=W(n-2)+n^c
+  * W(n)=W(n-2)+2(n^c)
+  * W(n)=W(n-k)+k(n^c)
+  * n=k
+  * W(n)=W(1)+n*n^c
+  * n*n^c=n^c+1=n^c
+  * Asymptotic upper bound is n^c
+  * 𝑂(𝑛𝑐+1)
 .  
 .  
 .  
 .  
 .  
   * $W(n)=W(\sqrt{n})+1$
+  * W(sqrt(n))=W(n^1/4)+1
+  * W(n)=W(n^1/4)+1+1
+  * W(n)=W(n^1/2^k)+k
+  * n^1/2^k=1
+  * n=1^2^k
+  * log(n)=2^k
+  * log(log(n))=k
+  * W(n)=W(1)+  log(log(n))
+  * Asymptotic upper bound is log(log(n))
+  * 𝑂(lglg𝑛)
 
 
 2. Suppose that for a given task you are choosing between the following three algorithms:
@@ -67,19 +158,38 @@ and push to your github repository.
   * Algorithm $\mathcal{A}$ solves problems by dividing them into
       five subproblems of half the size, recursively solving each
       subproblem, and then combining the solutions in linear time.
-    
+W(n) = 5W(n/2) + n
+W(n/2)= 5W(n/4) + n
+W(n)=5*5W(n/4) +2n
+W(n)=5^k*W(n/2^k)+kn
+log2(n)=k
+W(n)=5^log2(n)*W(1)+log2(n)*n
+Asymptotic run time is nlog(n)
   * Algorithm $\mathcal{B}$ solves problems of size $n$ by
       recursively solving two subproblems of size $n-1$ and then
       combining the solutions in constant time.
-    
+W(n) = 2W(n-1) + c
+W(n-1)=2W(n-2)+c
+W(n)=2*2W(n-2)+c+c
+W(n)=2^kW(n-k)+ kc
+n=k
+W(n)=2^n*W(1)+nc
+Asymptotic run time is 2^n
   * Algorithm $\mathcal{C}$ solves problems of size $n$ by dividing
       them into nine subproblems of size $n/3$, recursively solving
       each subproblem, and then combining the solutions in $O(n^2)$
       time.
-
+W(n)= 9W(n/3) + n^2
+W(n/3)=9W(n/3)+n^2
+W(n)=9*9W(n/9)+2(n^2)
+W(n)=9^k*W(n/3^k)+kn^2
+log3(n)=k
+W(n)=9^log3(n)*W(1)+log3(n)*n^2
+W(n)=3n+log3(n)*n^2
+Asymptotic run time is log3(n)*n^2
     What are the asymptotic running times of each of these algorithms?
     Which algorithm would you choose?
-
+I would choose the first algorithim with run time nlog(n). nlog(n) is less than log3(n)*n^2 is less than 2^n
 
 3. Now that you have some practice solving recurrences, let's work on
   implementing some algorithms. In lecture we discussed a divide and
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-"""
-CMPS 2200  Assignment 2.
-See assignment-02.pdf for details.
-"""
 import time
+import tabulate
 
 class BinaryNumber:
     """ done """
     def __init__(self, n):
         self.decimal_val = n               
-        self.binary_vec = list('{0:b}'.format(n)) 
+        self.binary_vec = list('{0:b}'.format(n))
         
     def __repr__(self):
         return('decimal=%d binary=%s' % (self.decimal_val, ''.join(self.binary_vec)))
@@ -42,23 +39,132 @@ def pad(x,y):
         x = ['0'] + x
         y = ['0'] + y
     return x,y
+    
+def quadratic_multiply(x, y):
+    # this just converts the result from a BinaryNumber to a regular int
+    return _quadratic_multiply(x,y).decimal_val
 
+def _quadratic_multiply(x, y):
+    xvec = x.binary_vec
+    yvec = y.binary_vec
+    xvec, yvec = pad(xvec, yvec)
+    #print('   '*len(xvec), xvec, yvec)
+    if x.decimal_val <= 1 and y.decimal_val <= 1:
+        return BinaryNumber(x.decimal_val * y.decimal_val)
+    
+    # 4 recursive calls
+    x_left, x_right = split_number(xvec)
+    y_left, y_right = split_number(yvec)
+    # x_L * y_L
+    left_product = _quadratic_multiply(x_left, y_left)
+    # x_R * y_R
+    right_product = _quadratic_multiply(x_right, y_right)
+    # x_L * y_R
+    left_right_product = _quadratic_multiply(x_left, y_right)
+    # x_R * y_L
+    right_left_product = _quadratic_multiply(x_right, y_left)
+    
+    # O(n) addition: x_L*y_R + x_R*y_L
+    middle_term = BinaryNumber(left_right_product.decimal_val +
+                               right_left_product.decimal_val)
+    # 2^{n/2} (x_L*y_R + x_R*y_L)
+    middle_term = bit_shift(middle_term, len(xvec)//2)
+    
+    # 2^n (x_L * y_L)
+    left_product = bit_shift(left_product, len(xvec))
+    
+    # O(n) addition
+    return BinaryNumber(left_product.decimal_val +
+                        middle_term.decimal_val +
+                        right_product.decimal_val)
 
-
-def subquadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
 
 ## Feel free to add your own tests here.
 def test_multiply():
+    assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert quadratic_multiply(BinaryNumber(8), BinaryNumber(8)) == 8*8
+    assert quadratic_multiply(BinaryNumber(9), BinaryNumber(8)) == 9*8
+    assert quadratic_multiply(BinaryNumber(10), BinaryNumber(10)) == 10*10
+
+test_multiply()
+#quadratic_multiply(BinaryNumber(8), BinaryNumber(9))
+def subquadratic_multiply(x, y):
+    return _subquadratic_multiply(x,y).decimal_val
+    
+def _subquadratic_multiply(x,y):
+    xvec = x.binary_vec
+    yvec = y.binary_vec
+    xvec, yvec = pad(xvec, yvec)
+#     print('  ' * len(xvec), xvec, yvec)
+    if x.decimal_val <= 1 and y.decimal_val <= 1:
+        return BinaryNumber(x.decimal_val * y.decimal_val)
+    
+    # 4 recursive calls
+    x_left, x_right = split_number(xvec)
+    y_left, y_right = split_number(yvec)
+    # x_L * y_L
+    left_product = _subquadratic_multiply(x_left, y_left)
+    # x_R * y_R
+    right_product = _subquadratic_multiply(x_right, y_right)
+    # x_L + x_R
+    xsum = BinaryNumber(x_left.decimal_val + x_right.decimal_val)
+    # y_L + y_R
+    ysum = BinaryNumber(y_left.decimal_val + y_right.decimal_val)
+    # recursive call plus O(n) addition
+    # (x_L + x_R) * (y_L + y_R) - (x_L * y_L) - (x_R * y_R)
+    middle_term = BinaryNumber(_subquadratic_multiply(xsum, ysum).decimal_val -
+                               left_product.decimal_val -
+                               right_product.decimal_val)
+    # 2^{n/2}  (x_L*y_R + x_R*y_L)
+    middle_term = bit_shift(middle_term, len(xvec)//2)
+    # 2^n (x_L * y_L)
+    left_product = bit_shift(left_product, len(xvec))
+    # O(n) addition
+    ret = BinaryNumber(left_product.decimal_val + middle_term.decimal_val + right_product.decimal_val)
+    return ret
+    
+
+## Feel free to add your own tests here.
+def test_subquadratic_multiply():
     assert subquadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert subquadratic_multiply(BinaryNumber(8), BinaryNumber(8)) == 8*8
+    assert subquadratic_multiply(BinaryNumber(9), BinaryNumber(8)) == 9*8
+    assert subquadratic_multiply(BinaryNumber(10), BinaryNumber(10)) == 10*10
+
+# subquadratic_multiply(BinaryNumber(8), BinaryNumber(9))
+test_subquadratic_multiply()
+
 
 def time_multiply(x, y, f):
     start = time.time()
     # multiply two numbers x, y using function f
+    f(x,y)
     return (time.time() - start)*1000
+    
+def compare_multiply():
+    res = []
+    ns = [10,100,1000,10000,100000,1000000,10000000,100000000,1000000000]
+    for n in ns:
+        qtime = time_multiply(BinaryNumber(n), BinaryNumber(n), quadratic_multiply)
+        subqtime = time_multiply(BinaryNumber(n), BinaryNumber(n), subquadratic_multiply)        
+        res.append((n, qtime, subqtime))
+    print_results(res)
+    
+    plt.figure()
+    plt.plot(ns, [r[1] for r in res], 'o-', label='quadratic')
+    plt.plot(ns, [r[2] for r in res], 'o-', label='subquadratic')
+    plt.legend()
+    plt.show()
 
+
+def print_results(results):
+    print("\n")
+    print(
+        tabulate.tabulate(
+            results,
+            headers=['n', 'quadratic', 'subquadratic'],
+            floatfmt=".3f",
+            tablefmt="github"))
     
     
-
+compare_multiply()
